3f99d9e26f47e7c48fb871b8fe858dc87e27b318
ynawhocodes <nw1398@naver.com>
2020-10-16 17:21:16 +0900

    merge 후 첫 commit
    - feature/ourdiary랑 합치고 runserver 가능하게 코드 고침

--- plantaryProject/project/urls.py
+++ plantaryProject/project/urls.py
@@ -1,22 +1,15 @@
 from django.contrib import admin
-<<<<<<< HEAD
 from django.urls import path, include
 import main.views
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', main.views.home, name="home"),
-    path('accounts/', include('allauth.urls')),
-]
-=======
-from django.urls import path
 from ourdiary import views
 from django.conf import settings
 from django.conf.urls.static import static
 
-urlpatterns = [
+urlpatterns = [  
+    path('', main.views.home, name="home"),
+    path('accounts/', include('allauth.urls')),
     path('admin/', admin.site.urls),
-    path('', views.ourdiary, name="ourdiary"),
+    path('ourdiary/', views.ourdiary, name="ourdiary"),
     path('ourdiary/<int:ourdiary_id>', views.detail, name="detail"),
     #path('like/<int:ourdiary_id>/', views.post_like, name="post_like"),
     path('like/', views.post_like, name="post_like"),

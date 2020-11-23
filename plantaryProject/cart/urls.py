28d5aa924b858d9c92aed294089d23576790c574
brina-lee <brina.lny@gmail.com>
2020-11-23 15:05:57 +0900

    store ver.2

--- plantaryProject/cart/urls.py
+++ plantaryProject/cart/urls.py
@@ -11,4 +11,6 @@ urlpatterns = [
     path('', views.cart_home, name='cart_home'),
     path('cart/', views.cart_detail, name='cart_detail'),
     path('add/<int:product_id>/', views.add_cart, name='add_cart'),
+    path('remove/<int:product_id>/', views.cart_remove, name='cart_remove'),
+    path('full_remove/<int:product_id>/', views.full_remove, name='full_remove'),
 ]
